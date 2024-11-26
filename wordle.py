39597d30455d910d4c10ec7464b987b8e2ef8cf8
nainghtetlinn <naing.work.it@gmail.com>
2024-11-26 21:08:31 +0630

    update wordle

--- wordle.py
+++ wordle.py
@@ -8,19 +8,17 @@ RESET = '\033[0m'
 
 class Printabel_word:
     def __init__(self, word, correct) :
-        self.word = word.upper()
-        self.correct = correct.upper()
+        self.result = ""
+        for i, char in enumerate(word):
+            if char == correct[i]:
+                self.result += GREEN + char + RESET
+            elif char in correct:
+                self.result += YELLOW + char + RESET
+            else:
+                self.result += LIGHT_GRAY + char + RESET
 
     def __str__(self) -> str:
-        result = ""
-        for i, char in enumerate(self.word):
-            if char == self.correct[i]:
-                result += GREEN + char + RESET
-            elif char in self.correct:
-                result += YELLOW + char + RESET
-            else:
-                result += LIGHT_GRAY + char + RESET
-        return result
+        return self.result
 
 def choose_word():
     with open('wordle-dataset.txt') as f:
@@ -31,7 +29,7 @@ def main():
     correct_word = choose_word()
     attempts = []
 
-    for _ in range(5):
+    for _ in range(10):
         while True:
             guess_word = input("Enter your guess: ")
             if len(guess_word) == 5:
@@ -39,7 +37,7 @@ def main():
             else:
                 print('Invalid input. Please enter a 5-letter word.')
     
-        word = Printabel_word(guess_word, correct_word)
+        word = Printabel_word(guess_word.upper(), correct_word.upper())
         attempts.append(word)
 
         print()
